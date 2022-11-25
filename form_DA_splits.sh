ab66db70488b878d7b15408f570bf3c67f99247a
Amr Keleg <amr_mohamed@live.com>
2022-11-25 14:47:19 +0000

    Download different files of DIA2MSA

--- form_DA_splits.sh
+++ form_DA_splits.sh
@@ -1,5 +1,11 @@
 wget -c "https://alt.qcri.org/~hmubarak/EGY-MGR-LEV-GLF-2-MSA.zip"
+wget -c "https://alt.qcri.org/~hmubarak/EGY-MGR-LEV-GLF-StrongWords.zip"
+wget -c "http://alt.qcri.org/~hmubarak/EGY2MSA-sample-correction.zip"
 mkdir -p data/DIA2MSA/
 mv EGY-MGR-LEV-GLF-2-MSA.zip data/DIA2MSA
+mv EGY-MGR-LEV-GLF-StrongWords.zip data/DIA2MSA
+mv EGY2MSA-sample-correction.zip data/DIA2MSA
 cd data/DIA2MSA
 unzip EGY-MGR-LEV-GLF-2-MSA.zip
+unzip EGY-MGR-LEV-GLF-StrongWords.zip
+unzip EGY2MSA-sample-correction.zip
