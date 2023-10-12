42a80158555a604b6a2bf4d6ecf6391649d3bf31
Amr Keleg <amr_mohamed@live.com>
2023-10-12 15:27:27 +0100

    Update Table 4 code

--- assets/table4.py
+++ assets/table4.py
@@ -2,12 +2,14 @@ import pandas as pd
 from utils import commasize_number
 
 
-def generate_stats(df, split="Train"):
+def generate_stats(df, split="train"):
     d = df["source"].value_counts(dropna=False).to_dict()
     total_samples = df.shape[0]
 
     sources = ["alghad_c", "alghad_a", "alriyadh_c", "alriyadh_a", "youm7_c", "youm7_a"]
-    print("split &", " & ".join(sources))
+
+    if split == "train":
+        print("split &", " & ".join(sources))
     table_line = f"\\textbf{{{split.capitalize()}}}"
     for source in sources:
         table_line += f" & {commasize_number(d[source])}"
@@ -17,5 +19,9 @@ def generate_stats(df, split="Train"):
 
 
 if __name__ == "__main__":
+    no_samples = 0
     for split in ["train", "dev", "test"]:
-        generate_stats(pd.read_csv(f"../data/AOC/{split}.tsv", sep="\t"), split=split)
+        df = pd.read_csv(f"../data/AOC/{split}.tsv", sep="\t")
+        generate_stats(df, split=split)
+        no_samples += df.shape[0]
+    print("\nTotal number of samples:", no_samples)
